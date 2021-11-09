b2c36d562e447eefa82d3ca9dd186ff811349224
Thomas Witherow <twitherow@bellsouth.net>
2021-11-09 15:54:54 -0600

    initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+Console.WriteLine("How many eggs will the sisters get?");
+int numbOfEggs = int.Parse(Console.ReadLine());
+
+
+
+ int sistersShare = numbOfEggs / 4;
+int duckbearsShare = numbOfEggs % 4;
+
+Console.WriteLine($"The sisters get {sistersShare} and duckbear gets {duckbearsShare}.");
