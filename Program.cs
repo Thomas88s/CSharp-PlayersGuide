718643684c7822497d3ad6b67ff378b45f0dede9
Thomas Witherow <twitherow@bellsouth.net>
2021-11-09 16:35:55 -0600

    added power calculator

--- Program.cs
+++ Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
 
 
-Console.WriteLine("How many eggs will the sisters get?");
-int numbOfEggs = int.Parse(Console.ReadLine());
+//Console.WriteLine("How many eggs will the sisters get?");
+//int numbOfEggs = int.Parse(Console.ReadLine());
 
 
 
- int sistersShare = numbOfEggs / 4;
-int duckbearsShare = numbOfEggs % 4;
+// int sistersShare = numbOfEggs / 4;
+//int duckbearsShare = numbOfEggs % 4;
 
-Console.WriteLine($"The sisters get {sistersShare} and duckbear gets {duckbearsShare}.");
+//Console.WriteLine($"The sisters get {sistersShare} and duckbear gets {duckbearsShare}.");
+
+Console.WriteLine("How many provinces do you own Melik?");
+int melickAnswer1 = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("How many duchies do you own Melik?");
+int melickAnswer2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("How many estates do you own Melik?");
+int melickAnswer3 = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("How many provinces do you own Casik?");
+int casikAnswer1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("How many duchies do you own Casik?");
+int casikAnswer2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("How many estates do you own Casik?");
+int casikAnswer3 = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("How many provinces do you own balik?");
+int balikAnswer1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("How many duchies do you own balik?");
+int balikAnswer2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("How many estates do you own balik?");
+int balikAnswer3 = int.Parse(Console.ReadLine());
+
+
+int estate = 1;
+int duchy = 3;
+int province = 6;
+
+Console.WriteLine($"Melick's total power is {(melickAnswer1 * estate) + (melickAnswer2 * duchy) + (melickAnswer3 * province) + '.'}");
+Console.WriteLine($"Melick's total power is {(casikAnswer1 * estate) + (casikAnswer2 * duchy) + (casikAnswer3 * province) + '.'}");
+Console.WriteLine($"Melick's total power is {(balikAnswer1 * estate) + (balikAnswer2 * duchy) + (balikAnswer3 * province) + '.'}");
