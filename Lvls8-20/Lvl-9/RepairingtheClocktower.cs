4a5095c5cc118bfe641fb90d5272c80d9beeb9c9
Thomas Witherow <twitherow@bellsouth.net>
2021-11-10 13:47:37 -0600

    fixed the clock

---
+++ Lvls8-20/Lvl-9/RepairingtheClocktower.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+
+
+
+Console.Write("Enter a number into the clock. ");
+int clockInput = Convert.ToInt32(Console.ReadLine());
+
+if (clockInput%2 == 0)
+{
+    Console.WriteLine("Tick");
+}
+else
+{
+    Console.WriteLine("Tock");
+}
