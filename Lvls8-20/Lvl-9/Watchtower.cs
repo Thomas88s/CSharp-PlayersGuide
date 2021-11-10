d445cec61abf8e2584a2525babe863df2e7ea4bc
Thomas Witherow <twitherow@bellsouth.net>
2021-11-10 15:25:10 -0600

    added a watchtower

---
+++ Lvls8-20/Lvl-9/Watchtower.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+
+Console.WriteLine("Help locate the enemy!");
+Console.Write("What is the x value of the enemy location? ");
+int xCoord = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Now, what is the y value of the enemy location? ");
+int yCoord = Convert.ToInt32(Console.ReadLine());
+
+if (xCoord < 0 && yCoord > 0)
+{
+    Console.WriteLine("They're approaching from the North West"!);
+}
+else if (xCoord < 0 && yCoord == 0)
+{
+    Console.WriteLine("They're approaching from the West"!);
+}
+else if (xCoord < 0 && yCoord < 0)
+{
+    Console.WriteLine("They're approaching from the South West"!);
+}
+else if (xCoord == 0 && yCoord > 0)
+{
+    Console.WriteLine("They're approaching from the North"!);
+}
+else if (xCoord == 0 && yCoord == 0)
+{
+    Console.WriteLine("The enemy is here!"!);
+}
+else if (xCoord == 0 && yCoord < 0)
+{
+    Console.WriteLine("They're approaching from the South"!);
+}
+else if (xCoord > 0 && yCoord > 0)
+{
+    Console.WriteLine("They're approaching from the North East"!);
+}
+else if (xCoord > 0 && yCoord == 0)
+{
+    Console.WriteLine("They're approaching from the East"!);
+}
+else if (xCoord > 0 && yCoord < 0)
+{
+    Console.WriteLine("They're approaching from the South West"!);
+}
