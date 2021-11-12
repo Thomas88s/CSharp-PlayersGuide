9340a81dd4be8135d4a90b4bcc908f330a61d334
Thomas Witherow <twitherow@bellsouth.net>
2021-11-11 18:38:15 -0600

    replicator works and foreach loops

---
+++ Lvls8-20/Lvl-12/TheLawsofFreach.cs
@@ -0,0 +1,30 @@
+﻿//using System;
+
+
+
+
+//int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+
+//int currentMinimum = int.MaxValue;
+
+//    foreach (int item in array)
+//{
+//    if (item < currentMinimum)
+//        currentMinimum = item;
+//}
+
+//Console.WriteLine(currentMinimum);
+
+//Console.WriteLine("---------------");
+
+//int[] newArray = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+
+//int total = 0;
+
+//    foreach (int item in newArray)
+//{
+//    total += item;
+//}
+
+//float average = (float)total / newArray.Length;
+//Console.WriteLine(average);
