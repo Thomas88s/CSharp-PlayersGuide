6351a8936ff87ca2cb3d069175b44be7754428f5
Thomas Witherow <twitherow@bellsouth.net>
2021-11-15 11:52:13 -0600

    forgot to merge main

--- Lvls8-20/Lvl-11/ThePrototype.cs
+++ Lvls8-20/Lvl-11/ThePrototype.cs
@@ -1,48 +1,48 @@
-﻿using System;
+﻿//using System;
 
-int number1 = AskForNumber("Please enter a number between 0 and 100, Pilot: ");
-int number2 = AskForNumber("Please enter a number between 0 and 100, Hunter: ");
+//int number1 = AskForNumber("Please enter a number between 0 and 100, Pilot: ");
+//int number2 = AskForNumber("Please enter a number between 0 and 100, Hunter: ");
 
-int pilotNumb = number1;
-int hunterNumb = number2;
-
-
-int AskForNumber(string text)
-{
-    Console.Write(text + " ");
-    int number = Convert.ToInt32(Console.ReadLine());
-    return number;
-}
-
-Console.Write(number1);
+//int pilotNumb = number1;
+//int hunterNumb = number2;
 
 
-Console.Clear();
+//int AskForNumber(string text)
+//{
+//    Console.Write(text + " ");
+//    int number = Convert.ToInt32(Console.ReadLine());
+//    return number;
+//}
 
-Console.Write(number2);
+//Console.Write(number1);
 
 
-while (hunterNumb != pilotNumb)
-{
+//Console.Clear();
 
-    if (hunterNumb < pilotNumb)
-    {
-        Console.WriteLine("Hunter number is too low. Please try again. ");    
-        hunterNumb = Convert.ToInt32(Console.ReadLine());
+//Console.Write(number2);
+
+
+//while (hunterNumb != pilotNumb)
+//{
+
+//    if (hunterNumb < pilotNumb)
+//    {
+//        Console.WriteLine("Hunter number is too low. Please try again. ");    
+//        hunterNumb = Convert.ToInt32(Console.ReadLine());
         
-    }
-    else if (hunterNumb > pilotNumb)
-    {
-        Console.WriteLine("Hunter number is too high. Please try again. ");
-        hunterNumb = Convert.ToInt32(Console.ReadLine());
-    }
-    else
-    {
-        break;
-    }
+//    }
+//    else if (hunterNumb > pilotNumb)
+//    {
+//        Console.WriteLine("Hunter number is too high. Please try again. ");
+//        hunterNumb = Convert.ToInt32(Console.ReadLine());
+//    }
+//    else
+//    {
+//        break;
+//    }
 
-}
-Console.WriteLine("The hunter's number matches the pilot's number. Correct.");
+//}
+//Console.WriteLine("The hunter's number matches the pilot's number. Correct.");
 
 
 
---
+++ Lvls8-20/Lvl-13/Countdown.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+//for (int x = 10; x > 0; x--)
+//    Console.WriteLine(x);
+int number = 10;
+
+int loopDLoop(int number)
+{
+    if (number == 1) return number;
+    Console.WriteLine(number);
+    return  loopDLoop(number - 1);
+
+}
+
+Console.WriteLine(loopDLoop(number));
