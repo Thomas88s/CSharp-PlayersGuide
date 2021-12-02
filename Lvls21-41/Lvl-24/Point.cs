8c6a45cb48b4c98186561a5224724c9b216bc1f8
Thomas Witherow <twitherow@bellsouth.net>
2021-12-02 11:57:49 -0600

    first part done

---
+++ Lvls21-41/Lvl-24/Point.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+Point a = new Point(2, 3);
+Point b = new Point(-4, 0);
+
+Console.Write($"({a.X}, {a.Y})");
+Console.Write($"({b.X}, {b.Y})");
+
+
+public class Point
+{
+    public float X { get; }
+    public float Y { get; }
+
+    public Point(float x, float y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public Point() : this(0, 0) { }
+
+}
+
---
+++ Practice.cs
@@ -0,0 +1,23 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace HelloWorld
+//{
+//    class Practice
+//    {
+//        List<int> list = new List<int> { 1, 3, 9, 6, 3, 10, 76, 54 };
+//        public static printAMedian(List<int> n)
+//        {
+
+//        }
+
+
+//        Console.WriteLine(printAMedian(list));
+//    }
+
+
+
+//}
